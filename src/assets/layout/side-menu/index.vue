03bbba5a6b1c0e3a7c8054dc93bd565c1e84b9f0
kakahikari <kakahikari@gmail.com>
2017-03-29 14:48:32 +0800

    side-menu

--- src/assets/layout/side-menu/index.vue
+++ src/assets/layout/side-menu/index.vue
@@ -1,61 +1,56 @@
 <template lang="pug">
   nav
+    div.text-muted.nav-title {{ $root.i18n('Navigation') }}
     ul.nav.nav-pills.flex-column
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
-      li.nav-item hi
-      li.nav-item hi2
-      li.nav-item hi3
-      li.nav-item hi3
+      template(v-for="item in list")
+        node
+          template(slot="icon")
+            icon(":name"="iconList[item.name]")
+          template(slot="text") {{ item.text }}
+          template(slot="child" v-if="item.child.length > 0")
+            template(v-for="child in item.child")
+              li.nav-item__child__link("v-waves.block"="") {{ child.text }}
 </template>
 
 <script>
-export default {
-  name: 'side-menu'
-}
+  import node from './node'
+  import UserService from 'services/userService'
+  import helper from 'helper'
+
+  export default {
+    name: 'side-menu__index',
+
+    data () {
+      return {
+        list: [],
+        iconList: {
+          member: 'user',
+          finance: 'money',
+          agent: 'users',
+          report: 'file-text-o',
+          rate_money: 'gift'
+        }
+      }
+    },
+
+    mounted () {
+      this.$nextTick(() => {
+        const params = {context: this}
+        UserService.getFieldList(params).then((res) => {
+          this.list = res
+        })
+        .catch((err) => {
+          helper.log(err)
+        })
+      })
+    },
+
+    components: {
+      node
+    }
+  }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  @import "./style";
 </style>
---
+++ src/assets/layout/side-menu/node.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  li.nav-item
+    .nav-item__title(@click="toggle()" "v-waves.block"="")
+      slot(name="icon")
+      slot(name="text")
+    transition(name="slide")
+      ul.nav-item__child(v-if="active")
+        slot(name="child")
+</template>
+
+<script>
+export default {
+  name: 'side-menu__node',
+
+  data () {
+    return {
+      active: false
+    }
+  },
+
+  methods: {
+    toggle () {
+      this.active = !this.active
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "./style";
+</style>
