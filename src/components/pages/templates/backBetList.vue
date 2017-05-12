7c0e1abc8670f4558fa7e1917d2860f2053e7167
kakahikari <kakahikari@gmail.com>
2017-05-12 20:49:24 +0800

    backBetList邏輯更新

--- src/components/pages/templates/backBetList.vue
+++ src/components/pages/templates/backBetList.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
   .card(v-if="mainList.length > 0")
     .card-block
-      b-table.table-bordered(striped ":per-page"="Number(formData.page_size)" ":items"="mainList" ":fields"="mainFields")
-      template(v-if="kind == '10' && backList10.length > 0")
-        b-table.table-bordered(striped ":per-page"="Number(formData.page_size)" ":items"="backList10" ":fields"="backFields10")
-      template(v-else-if="backList.length > 0")
-        b-table.table-bordered(striped ":per-page"="Number(formData.page_size)" ":items"="backList" ":fields"="backFields")
+      .card-text.text-right
+        .text-muted {{ $root.i18n('Compute kind') }}: {{$root.i18n(computeKindList[kind])}}
+      b-table.table-bordered(striped ":per-page"="Number(formData.page_size)" ":items"="mainList" ":fields"="mainFields[kind]")
+        template(slot="agent_name" scope="item")
+          template(v-if="item.value == '' || item.value == null")
+            | {{ $root.i18n('total') }}
+          template(v-else)
+            | {{ item.value }}
+      template(v-if="backList.length > 0")
+        b-table.table-bordered(striped ":per-page"="Number(formData.page_size)" ":items"="backList" ":fields"="backFields[kind]")
           template(slot="kind_code" scope="item")
-            | {{ $root.i18n(item.value) }}
+            | {{ $root.i18n(kindCodeList[item.value]) }}
 </template>
 
 <script>
   import ReportService from 'services/reportService'
+  import { computeKindList, kindCodeList } from 'src/xhrConfig'
 
   export default {
     name: 'templates__backBetList',
@@ -21,32 +27,129 @@
         currentPage: 1,
         mainList: [],
         mainFields: {
-          agent_name: { label: this.$root.i18n('agent'), sortable: true },
-          kind_code: { label: this.$root.i18n('4 categories'), sortable: true },
-          sum_bet: { label: this.$root.i18n('combined valid bet'), sortable: true },
-          profit: { label: this.$root.i18n('profits (valid bet-payout)'), sortable: true },
-          back_money: { label: this.$root.i18n('amount of return commission rate'), sortable: true },
-          cheap_money: { label: this.$root.i18n('promotion'), sortable: true },
-          fee_money: { label: this.$root.i18n('processing fees'), sortable: true },
-          rake_money: { label: this.$root.i18n('member rebate') },
-          Sub_total: { label: this.$root.i18n('return commission tally'), sortable: true }
+          0: {
+            agent_name: { label: this.$root.i18n('agent') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          1: {
+            agent_name: { label: this.$root.i18n('agent') },
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          2: {
+            agent_name: { label: this.$root.i18n('agent') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          3: {
+            agent_name: { label: this.$root.i18n('agent') },
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          4: {
+            agent_name: { label: this.$root.i18n('agent') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          5: {
+            agent_name: { label: this.$root.i18n('agent') },
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          },
+          10: {
+            agent_name: { label: this.$root.i18n('agent') },
+            number: { label: this.$root.i18n('valid member') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            sum_bet: { label: this.$root.i18n('combined valid bet') },
+            profit: { label: this.$root.i18n('profits (valid bet-payout)') },
+            back_money: { label: this.$root.i18n('amount of return commission rate') },
+            cheap_money: { label: this.$root.i18n('promotion') },
+            fee_money: { label: this.$root.i18n('processing fees') },
+            rake_money: { label: this.$root.i18n('member rebate') },
+            Sub_total: { label: this.$root.i18n('return commission tally') }
+          }
         },
         backList: [],
         backFields: {
-          name: { label: this.$root.i18n('game company'), sortable: true },
-          money: { label: this.$root.i18n('profit'), sortable: true },
-          rate: { label: this.$root.i18n('agent'), sortable: true }
+          0: {
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          1: {
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          2: {
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          3: {
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          4: {
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          5: {
+            name: { label: this.$root.i18n('game company') },
+            kind_code: { label: this.$root.i18n('4 categories') },
+            rate: { label: this.$root.i18n('rate') },
+            money: { label: this.$root.i18n('profits') }
+          },
+          10: {
+            people_money: { label: this.$root.i18n('valid bet (condition)') },
+            people_num: { label: this.$root.i18n('valid member (condition)') },
+            rateEG: { label: this.$root.i18n('electronic return commission rate (profits)') },
+            rateLI: { label: this.$root.i18n('live return commission rate (profits)') },
+            rateLO: { label: this.$root.i18n('lottery comission rate (valid)') },
+            rateSP: { label: this.$root.i18n('sports return commission (profits)') }
+          }
         },
-        backList10: [],
-        backFields10: {
-          people_money: { label: this.$root.i18n('valid bet (condition)'), sortable: true },
-          people_num: { label: this.$root.i18n('valid member (condition)'), sortable: true },
-          rateEG: { label: this.$root.i18n('electronic return commission rate (profits)'), sortable: true },
-          rateLI: { label: this.$root.i18n('live return commission rate (profits)'), sortable: true },
-          rateLO: { label: this.$root.i18n('lottery comission rate (valid)'), sortable: true },
-          rateSP: { label: this.$root.i18n('sports return commission (profits)'), sortable: true }
-        },
-        kind: 0
+        kind: 0,
+        computeKindList: computeKindList,
+        kindCodeList: kindCodeList
       }
     },
 
@@ -67,7 +170,6 @@
       doRequest (formData) {
         this.mainList = []
         this.backList = []
-        this.backList10 = []
         const body = {}
         body.agent_id = formData.agent_id
         body.start_date = formData.start_date
@@ -75,15 +177,17 @@
         body.page_size = formData.page_size
         ReportService.getBackBetReport({context: this, body: body}).then((res) => {
           this.kind = res.kind
-          if (this.kind === '10') {
-            this.mainFields.number = { label: this.$root.i18n('number of active members'), sortable: true }
-            this.backList10 = res.back_set
-          } else {
-            delete this.mainFields['number']
-            this.backList = res.back_set
+          this.mainList = res.main_list
+          this.backList = res.back_set
+          if (this.kind === '1' || this.kind === '3' || this.kind === '5' || this.kind === '10') {
+            res.child_list.forEach((node) => {
+              var targetIndex = 0
+              this.mainList.forEach((target, index) => {
+                if (target.agent_name === node.agent_name) targetIndex = index
+              })
+              this.mainList.splice(targetIndex, 0, node)
+            })
           }
-          this.mainList.push(res.main_list[''])
-          this.mainList.push(res.main_list[0])
         })
         .catch((err) => {
           this.$root.showToast({type: 'warning', content: err})
